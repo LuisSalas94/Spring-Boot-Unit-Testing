d0cf97497edfadaf182019c5290d9f6247b2c118
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-21 08:20:32 -0500

    JUnit test for getting al students operation

--- src/test/java/net/fernandosalas/springboot/repository/StudentRepositoryTest.java
+++ src/test/java/net/fernandosalas/springboot/repository/StudentRepositoryTest.java
@@ -9,13 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @DataJpaTest
 public class StudentRepositoryTest {
     @Autowired
     private StudentRepository studentRepository;
 
-    // JUnit test for save employee operation | given-when-then
-    @DisplayName("JUnit test for save employee operation")
+    // JUnit test for save student operation | given-when-then
+    @DisplayName("JUnit test for save student operation")
     @Test
     public void givenStudentObject_whenSavingStudent_thenReturnStudentObject() {
         //given - precondition or setup
@@ -31,5 +35,36 @@ public class StudentRepositoryTest {
         assertThat(savedStudent).isNotNull();
         assertThat(savedStudent.getId()).isGreaterThan(0);
         assertThat(savedStudent.getFirstName()).isEqualTo("Fernando");
+    }
+
+    //JUnit test for getting al students operation
+    @DisplayName("JUnit test for getting al students operation")
+    @Test
+    public void givenStudentList_whenFindAllStudents_thenReturnStudentList() {
+        //given - precondition or setup
+        Student student = Student.builder()
+                .firstName("Fernando")
+                .lastName("Salas")
+                .email("fernando@gmail.com")
+                .build();
+
+        Student student2 = Student.builder()
+                .firstName("Claudia")
+                .lastName("Ramos")
+                .email("claudia@gmail.com")
+                .build();
+
+        List<Student> studentList = Arrays.asList(student, student2);
+        studentRepository.saveAll(studentList);
+
+        // when - action or the behavior we are going to test
+        List<Student> retrievedStudents = studentRepository.findAll();
+
+        // then - verify the output
+        assertThat(retrievedStudents.size()).isGreaterThan(0);
+        assertThat(retrievedStudents).contains(student, student2);
+        assertThat(retrievedStudents).isNotEmpty()
+                .hasSize(2)
+                .containsExactlyInAnyOrder(student, student2);
     }
 }
