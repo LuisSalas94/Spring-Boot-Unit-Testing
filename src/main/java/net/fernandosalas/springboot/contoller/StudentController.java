5084d8993bdb286c052bc9c6f56419508054e1a3
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-22 17:17:20 -0500

    Test for getStudentById

--- src/main/java/net/fernandosalas/springboot/contoller/StudentController.java
+++ src/main/java/net/fernandosalas/springboot/contoller/StudentController.java
@@ -1,9 +1,11 @@
 package net.fernandosalas.springboot.contoller;
+
 import lombok.AllArgsConstructor;
 import net.fernandosalas.springboot.entity.Student;
 import net.fernandosalas.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +28,13 @@ public class StudentController {
     @ResponseStatus(HttpStatus.OK)
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") long studentId) {
+        return studentService.getStudentById(studentId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
--- src/test/java/net/fernandosalas/springboot/controller/StudentControllerTest.java
+++ src/test/java/net/fernandosalas/springboot/controller/StudentControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -23,6 +24,8 @@ import static org.mockito.BDDMockito.*;
 
 @WebMvcTest
 public class StudentControllerTest {
+
+    private static final String API_PATH = "/api/students";
     @Autowired
     private MockMvc mockMvc;
     @MockBean
@@ -45,7 +48,7 @@ public class StudentControllerTest {
 
         // when - action or the behavior we are going to test
         // Simulating an HTTP POST request to "/api/students"
-        ResultActions response = mockMvc.perform(post("/api/students")
+        ResultActions response = mockMvc.perform(post(API_PATH)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(student)));
 
@@ -78,12 +81,35 @@ public class StudentControllerTest {
         given(studentService.getAllStudents()).willReturn(studentList);
 
         // when - action or the behavior we are going to test
-        ResultActions response = mockMvc.perform(get("/api/students"));
+        ResultActions response = mockMvc.perform(get(API_PATH));
 
         // then - verify the output
         response.andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("$.size()", is(2)));
+    }
+
+
+    @Test
+    public void givenStudentObject_whenGetStudentById_thenReturnStudentObject() throws Exception {
+        //given - precondition or setup
+        long studentId = 1L;
+        Student student = Student.builder()
+                .firstName("Fernando")
+                .lastName("Salas")
+                .email("fernando@gmail.com")
+                .build();
+        given(studentService.getStudentById(studentId)).willReturn(Optional.of(student));
+
+        // when - action or the behavior we are going to test
+        ResultActions response = mockMvc.perform(get(API_PATH + "/{id}", studentId));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(student.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(student.getLastName())))
+                .andExpect(jsonPath("$.email", is(student.getEmail())));
     }
 
 }
